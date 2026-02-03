e6225f2dbb476a9fb5b6ce82e1f77da502f9a896
bennynguyen0815-cyber <your-email@example.com>
2026-02-03 07:11:58 -0600

    Remove invalid turbo config from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 }
 
 module.exports = nextConfig
